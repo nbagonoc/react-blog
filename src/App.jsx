377cc8ba917ca01f21c26cc42ba888c6c8ae5a56
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-04-10 15:19:33 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='text-xl font-bold uppercase text-center text-blue-500 mt-10'>
+            hello tailwind
+        </h1>
+    </>
+  )
+}
+
+export default App
