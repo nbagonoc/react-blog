5060a583701aaaa80731a6cd26569cd7ea29a7ce
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2024-04-14 11:12:19 +0800

    now able to login

--- src/components/auths/login/AuthsLoginForm.jsx
+++ src/components/auths/login/AuthsLoginForm.jsx
@@ -1,6 +1,52 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { login } from '../../../redux/auths/authsSlice'
+
 const AuthsLoginForm = () => {
+    const dispatch = useDispatch()
+    const { isLoading, isError, isSuccess, message } = useSelector((state) => state.posts)
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleOnChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const loginData = {
+            email: formData.email,
+            password: formData.password,
+        }
+        dispatch(login(loginData))
+    }
+
   return (
-    <div>AuthsLoginForm</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleOnChange}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleOnChange}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
--- src/service/auths/authsService.js
+++ src/service/auths/authsService.js
@@ -4,9 +4,11 @@ import { authsAPI } from '../API.js'
 
 const login = async (data) => {
     try {
-        const response = await axios.get(`${authsAPI}/login`, data)
+        const response = await axios.post(`${authsAPI}/login`, data)
+        console.log(response)
         return response.data
     } catch (error) {
+        console.log(error)
         const message = error.response.data.message;
         throw new Error(message);
     }
@@ -15,8 +17,10 @@ const login = async (data) => {
 const register = async (data) => {
     try {
         const response = await axios.post(`${authsAPI}/register`, data)
+        console.log(response)
         return response.data.message
     } catch (error) {
+        console.log(error)
         const message = error.response.data;
         throw new Error(message);
     }
